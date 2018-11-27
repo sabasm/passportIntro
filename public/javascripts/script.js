a3e5c23f0c51e5b11544a2766abdbc1c1da80a44
Héctor BlisS <hectorbliss@MacBook-Pro-de-Hector.local>
2018-11-27 16:40:20 -0600

    axios

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
+//form
+document.querySelector("#form").addEventListener("submit", sendForm);
 
-  console.log('IronGenerator JS imported successfully!');
+function sendForm(e) {
+  e.preventDefault();
+  const restaurant = {
+    name: e.target.name.value,
+    location: {
+      type: "Point",
+      coordinates: [e.target.lng.value, e.target.lat.value]
+    }
+  };
+  axios
+    .post("http://localhost:3000/restaurants/api/new", restaurant)
+    .then(result => {
+      console.log(result);
+      alert("Creado");
+    })
+    .catch(e => console.log(e));
+}
 
-}, false);
+// setTimeout(apiRequest, 5000);
+// //javascript se comunica con el servidor JSON
+// function apiRequest() {
+//   axios
+//     .get("http://localhost:3000/restaurants/api/5bfd6268673bd907c4d381d8")
+//     .then(result => {
+//       console.log(result);
+//     })
+//     .catch(e => {
+//       console.log(e);
+//     });
+// }
--- routes/rests.js
+++ routes/rests.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Restaurant = require("../models/Restaurant");
 
+//json api
+
+router.post("/api/new", (req, res) => {
+  Restaurant.create(req.body).then(rest => {
+    res.status(201).json(rest);
+  });
+});
+
+router.get("/api/:id", (req, res) => {
+  Restaurant.findById(req.params.id).then(rest => {
+    res.status(200).json(rest);
+  });
+});
+
 router.get("/new", (req, res, next) => {
   res.render("rests/new");
 });
