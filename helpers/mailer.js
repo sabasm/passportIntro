260556155cf87a3f1f03dc5b6e71cee01f95c0e1
sabasm <smma1992@gmail.com>
2018-11-28 16:52:28 -0600

    mailer

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -10,7 +10,20 @@ const transport = nodemailer.createTransport({
 
 function welcomeMail(email, name){
   transport.sendMail({
-    
+    bcc:email,
+    subject:"Polloyon te da la bienvenida!",
+    from:"polloyon@pollito.com",
+    html:`
+    <h1>Bienvenido ${name}!</h1>
+    <p>Estamos felices de tenerte!</p>
+    `
+
+  })
+  .then(res=>{
+    console.log(res)
+  })
+  .catch(err=>{
+    console.log(e)
   })
 
 
